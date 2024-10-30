c55ab7b0966d32160e159341259f6d7477a026d0
sophiewl1 <sophie.weil@gmx.net>
2024-10-30 00:32:47 +0000

    new changes

--- script.js
+++ script.js
@@ -22,3 +22,52 @@ document.addEventListener('DOMContentLoaded', function() {
 
     scrollWord(); // starts the animation
 });
+
+
+// smooth scrolling when click on link 
+
+document.addEventListener("DOMContentLoaded", function () {
+    // Selektiere alle Links in der Projektübersicht, die mit "#" beginnen
+    const links = document.querySelectorAll('.project-links a[href^="#"]');
+
+    // Iteriere über alle gefundenen Links und füge ihnen den Event-Listener hinzu
+    links.forEach(link => {
+        const targetSection = document.querySelector(link.getAttribute('href'));
+        
+        if (targetSection) {
+            link.addEventListener('click', function (event) {
+                event.preventDefault();
+                targetSection.scrollIntoView({
+                    behavior: 'smooth'
+                });
+            });
+        }
+    });
+});
+
+
+
+// smooth scroll to top button
+
+document.addEventListener("DOMContentLoaded", function () {
+    const scrollToTopButton = document.getElementById("scrollToTopButton");
+
+    // Zeigt den Button an, wenn der Benutzer scrollt
+    window.addEventListener("scroll", function () {
+        if (window.scrollY > 300) { // Ab einer Scrollhöhe von 300px sichtbar
+            scrollToTopButton.style.display = "block";
+        } else {
+            scrollToTopButton.style.display = "none";
+        }
+    });
+
+    // Scrollt nach oben, wenn der Button geklickt wird
+    scrollToTopButton.addEventListener("click", function () {
+        window.scrollTo({
+            top: 0,
+            behavior: "smooth"
+        });
+    });
+});
+
+
